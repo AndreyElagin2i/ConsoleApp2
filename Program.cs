d4dda228adb8ef162ddf2c0446b5acda9511cc59
AndreyElagin2i <82924250+AndreyElagin2i@users.noreply.github.com>
2021-05-26 13:41:24 +0300

    программа1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,21 @@ namespace ConsoleApp2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int a;
+            double b = 0, c; 
+            for (a = 1; a <= 20; a++)
+            {
+                c = b;
+                b = 1 / Math.Pow(2, a);
+                b = Math.Round(c + b);
+            }
+            
+            Console.WriteLine($"S="+ b.ToString());
+            
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
         }
     }
 }
